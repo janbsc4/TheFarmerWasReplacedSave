a2eb589faf9980b11a7592a7b890e2523eb12afa
Jan <janbscholl@gmail.com>
2024-11-27 20:01:36 +0100

    Added pumpkin & sunflower code

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+clear()
+nrfields = get_world_size() ** 2
+
 def is_even(n):
 	return n % 2 == 0
+	
+def harvestif():
+	if can_harvest():
+		harvest()
 	
 def water():
 	if get_water() < 0.75:
@@ -13,56 +20,85 @@ def muevase():
 		move(North)
 		
 def tend():
-	if can_harvest():
-		harvest()
+	if get_entity_type() != Entities.Pumpkin and get_entity_type() != Entities.Sunflower:
+		harvestif()
 	water()
 	if get_entity_type() == None and get_ground_type() == Grounds.Soil:
 		till()
 	muevase()
 	
-def checkmegapumpkin():
-	a = get_world_size() ** 2
-	while can_harvest() == True and get_entity_type() == Entities.Pumpkin:
-		a -= 1
-		quick_print(a)
-		muevase()
-		if a == 0:
-			harvest()
-			break
-		
 def tillifneeded():
 	if get_ground_type() != Grounds.Soil:
 		till()
+		
+def checkmegapumpkin():
+	b = nrfields
+	while can_harvest() == True and get_entity_type() == Entities.Pumpkin:
+		b -= 1
+		quick_print(b)
+		if b == 0:
+			harvestif()
+		muevase()
+		
+def harvestSF():
+	a = num_items(Items.Power)
+	quick_print("Petals to be harvested:", measure())
+	harvestif()
+	trade(Items.Sunflower_Seed)					
+	plant(Entities.Sunflower)
+	b = num_items(Items.Power)
+	quick_print("Energy harvested:", b-a)	
+		
+#######################################		
 		
 while True:
 	while num_items(Items.Hay) < 1000:
 		tend()
 	
-	while num_items(Items.Wood) < 1000:
+	while num_items(Items.Wood) < 2000:
+		tend()
 		if is_even(get_pos_y()) == True and is_even(get_pos_x()) == True:
 			plant(Entities.Tree)
 		elif  is_even(get_pos_y()) == False and is_even(get_pos_x()) == False:
 			plant(Entities.Tree)
-		tend()
+
 	
-	while num_items(Items.Carrot) < 1000:
+	while num_items(Items.Carrot) < 2000:
+		tend()
 		tillifneeded()
 		if get_entity_type() != Entities.Carrots:
 			trade(Items.Carrot_Seed)
 			plant(Entities.Carrots)
-		tend()
-	
-	while num_items(Items.Pumpkin) < 1000:
-		tillifneeded()
+
+
+	while num_items(Items.Pumpkin) < 3000:
 		if get_entity_type() != Entities.Pumpkin:
 			trade(Items.Pumpkin_Seed)
+			tillifneeded()
 			plant(Entities.Pumpkin)
+		tend()
 		checkmegapumpkin()
 		
-#while num_items(Items.Power) < 1000:
-#	tillifneeded()
-#	if get_entity_type() != Entities.Sunflower:
-#		trade(Items.Sunflower_Seed)
-#		plant(Entities.Sunflower)
-#	if measure() != None:
-#		tend()
+		
+	while num_items(Items.Power) < 1500:
+		tillifneeded()
+		if get_entity_type() != Entities.Sunflower:	
+			trade(Items.Sunflower_Seed)
+			plant(Entities.Sunflower)
+		tend()
+		if get_entity_type() == Entities.Sunflower:
+			fieldPower = []
+			for i in range(nrfields):
+				if get_entity_type() == Entities.Sunflower:
+					fieldPower.append(measure())
+				muevase()
+				if measure() == 15:
+					harvestSF()
+					break
+			if len(fieldPower) == nrfields:
+				for i in range(nrfields):
+					if measure() == max(fieldPower) or measure() == 15:
+						harvestSF()
+						break
+					else:
+						muevase()
